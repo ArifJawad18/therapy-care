bc7098f55fcd0e7877e987fd4dcad9ad42b3a171
Arifjawad18 <arif.jawad18@gmail.com>
2022-11-13 17:52:45 +0600

    home data and blog finish

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -3,12 +3,11 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-           <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
-  <div className="grid grid-flow-col gap-4">
+           <footer  className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+    <div className="grid grid-flow-col gap-4">
     <a href="/"className="link link-hover">About us</a> 
     <a href="/"className="link link-hover">Contact</a> 
     <a href="/"className="link link-hover">Jobs</a> 
-    <a href="/"className="link link-hover">Press kit</a>
   </div> 
   <div>
     <div className="grid grid-flow-col gap-4">
