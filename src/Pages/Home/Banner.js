10d18a308465b80d4168029e59e25ea5346f165a
Arifjawad18 <arif.jawad18@gmail.com>
2023-01-06 22:21:11 +0600

    edit img

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import img from '../Home/Home/img.png';
 
 const Banner = () => {
     return (
@@ -7,7 +7,7 @@ const Banner = () => {
     <section className=" dark:text-dark">
 	<div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
 		<a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 ">
-			<img src="https://www.care2curephysiotherapy.com/wp-content/uploads/2019/09/physiotherapy_techniques.jpg" alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
+			<img src={img} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
 			<div className="p-6 space-y-2 lg:col-span-5">
 				<h3 className="text-2xl font-semibold sm:text-4xl ">Dr: Jane Aitken Professional Physiotherapist</h3>
 				<p>She is providing high quality Physiotherapy Services</p>
