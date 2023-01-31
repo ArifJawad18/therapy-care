a31b57f63b492287b07babf1d9f5d67fb7a8cfa2
Arifjawad18 <arif.jawad18@gmail.com>
2023-02-01 04:00:48 +0600

    jwt

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -3,90 +3,81 @@ import { toast, Toaster } from "react-hot-toast";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 import OrderRow from "./OrderRow";
 
+
 const Orders = () => {
-  const { user, signOut } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   const [orders, setOrders] = useState([]);
 
   useState(() => {
-    fetch(
-      `https://therapy-care-server.vercel.app/orders?email=${user?.email}`,
-      {
-        header: {
-          authorization: `Bearer ${localStorage.getItem("therapycare-token")}`,
-        },
-      }
-    )
-      .then((res) => {
-        if (res.status === 401 || res.status === 403) {
-          return signOut();
-        }
+    fetch(`https://therapy-care-server.vercel.app/orders?email=${user?.email}`)
+      .then(res => res.json())
+      .then(data => setOrders(data))
+ 
+  }, [user?.email]);
 
-        return res.json();
+  const handleDelete = id => {
+   
+      fetch(`https://therapy-care-server.vercel.app/orders/${id}`, {
+        method: "DELETE",
       })
-      .then((data) => {
-        console.log("receive", data);
-        setOrders(data);
-      });
-  }, [user?.email, signOut]);
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if(data.deletedCount > 0){
+            toast.success("Deleted Order Successfully");
+            const remaining = orders.filter((odr) => odr._id !== id);
+            setOrders(remaining);
+          }
+        });
+  }
 
-  const handleDelete = (id) => {
+  const handleStatusUpdate = id =>{
     fetch(`https://therapy-care-server.vercel.app/orders/${id}`, {
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        if (data.deletedCount > 0) {
-          toast.success("Deleted Order Successfully");
-          const remaining = orders.filter((odr) => odr._id !== id);
-          setOrders(remaining);
-        }
-      });
-  };
-
-  const handleStatusUpdate = (id) => {
-    fetch(`https://therapy-care-server.vercel.app/orders/${id}`, {
-      method: "PATCH",
+      method: 'PATCH',
       headers: {
-        "content-type": "application/json",
+        'content-type': 'application/json'
       },
-      body: JSON.stringify({ status: "Approved" }),
+      body: JSON.stringify({status: 'Approved'})
     })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        if (data.modifiedCount > 0) {
-          const remaining = orders.filter((odr) => odr._id !== id);
-          const approving = orders.find((odr) => odr._id === id);
-          approving.status = "Approved";
+    .then(res => res.json())
+    .then(data =>{
+      console.log(data);
+      if(data.modifiedCount > 0){
+        const remaining = orders.filter(odr => odr._id !== id);
+        const approving = orders.find(odr => odr._id === id);
+        approving.status ='Approved'
 
-          const newOrder = [approving, ...remaining];
-          setOrders(newOrder);
-        }
-      });
-  };
+        const newOrder = [approving, ...remaining];
+        setOrders(newOrder);
+      }
+    })
+
+  }
 
   return (
     <div>
       <div className="overflow-x-auto w-full">
         <table className="table w-full">
           <thead className="bg-gray-300">
-            <h2 className="text-2xl text-center text-blue-700">All Orders</h2>
+           <h2 className="text-2xl text-center text-blue-700">All Orders</h2>
           </thead>
           <th>
-            {orders.map((order) => (
-              <OrderRow
-                key={order._id}
-                order={order}
-                handleDelete={handleDelete}
-                handleStatusUpdate={handleStatusUpdate}
-              ></OrderRow>
-            ))}
-            <Toaster />
+            {
+              orders.map(order =><OrderRow
+              key={order._id}
+              order ={order}
+              handleDelete={handleDelete}
+              handleStatusUpdate={handleStatusUpdate}
+              ></OrderRow>)
+
+            }
+            <Toaster/>
           </th>
         </table>
       </div>
+
+    
     </div>
   );
 };
