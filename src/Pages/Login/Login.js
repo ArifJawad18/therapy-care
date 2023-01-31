7fd06d0d17769a9f34a52cb6eabe82383bca2097
Arifjawad18 <arif.jawad18@gmail.com>
2023-01-31 23:48:13 +0600

    jwt try

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
         console.log(user);
         form.reset();
         setError("");
-        // navigate("/");
+        navigate("/");
         
         const currentUser = {
           email: user.email
